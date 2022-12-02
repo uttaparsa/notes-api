86afaefec9cadacde472f7e363e071983af031f8
Parsa <salehipour.edu@gmail.com>
2022-12-02 20:40:34 +0330

    token lifetime

--- app/config/settings.py
+++ app/config/settings.py
@@ -170,8 +170,8 @@ REST_FRAMEWORK = {
 
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ACCESS_TOKEN_LIFETIME': timedelta(day=2),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
     # 'TOKEN_USER_CLASS': 'ava.authentication.user.CustomizedTokenUser'
     # 'ROTATE_REFRESH_TOKENS': False,
     # 'BLACKLIST_AFTER_ROTATION': False,
