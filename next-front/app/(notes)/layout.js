54418c08ed0d11fbffb5a755f79b218b66e99860
Parsa <salehipour.edu@gmai.com>
2024-07-25 22:12:49 -0600

    added title to layout

--- next-front/app/(notes)/layout.js
+++ next-front/app/(notes)/layout.js
@@ -95,6 +95,11 @@ export default function RootLayout({ children }) {
 
   return (
     <html lang="en">
+      <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Notes</title>
+      </head>
       <body className={inter.className}>
       <AuthContext.Provider value={{ isAuthenticated, setIsAuthenticated }}>
       <NoteListContext.Provider value={noteLists}>
