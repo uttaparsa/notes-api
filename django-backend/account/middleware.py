be29ce02ba07f58b2c4486f768b36a83c641873a
Parsa <salehipour.edu@gmai.com>
2024-11-02 17:46:13 -0600

    save async

--- django-backend/account/middleware.py
+++ django-backend/account/middleware.py
@@ -1,19 +1,21 @@
+from asgiref.sync import sync_to_async
 from django.utils import timezone
-from .models import UserSession
+from .models import UserSession  # Ensure you have the correct path to your UserSession model
 
 class UpdateLastAccessMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
 
-    def __call__(self, request):
+    async def __call__(self, request):
         # Only update if the user has a session
         session_key = request.session.session_key
         if session_key:
-            # Get or create a SessionAccess object for this session key
-            session_access, created = UserSession.objects.get_or_create(session=session_key)
+            # Get or create a SessionAccess object for this session key asynchronously
+            session_access, created = await sync_to_async(UserSession.objects.get_or_create)(session=session_key)
             # Update the last accessed time
             session_access.last_activity = timezone.now()
-            session_access.save()
+            # Save asynchronously
+            await sync_to_async(session_access.save)()
 
-        response = self.get_response(request)
+        response = await sync_to_async(self.get_response)(request)
         return response
