ea522f687c74f46b0008b196b9533580d4a6fc3f
Parsa <salehipour.edu@gmai.com>
2025-12-18 17:09:45 -0600

    decreased concurrency and race conditions

--- django-backend/config/celery.py
+++ django-backend/config/celery.py
@@ -12,6 +12,11 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 # Auto-discover tasks in all installed apps
 app.autodiscover_tasks()
 
+# Prevent duplicate task execution
+app.conf.worker_prefetch_multiplier = 1
+app.conf.task_acks_late = True
+app.conf.task_reject_on_worker_lost = True
+
 @app.task(bind=True, ignore_result=True)
 def debug_task(self):
     print(f'Request: {self.request!r}')
