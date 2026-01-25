b87b749e83425b15f7d22301e10881239b0c5d72
Parsa <salehipour.edu@gmai.com>
2026-01-25 14:52:56 -0600

    script debug

--- scripts/dev_tools.sh
+++ scripts/dev_tools.sh
@@ -5,13 +5,13 @@ source ./.env.dev
 set +a
 
 if [ "$1" == "run" ]; then
-    docker compose -f ./docker-compose.dev.yml up 
+    docker compose -f ./docker-compose.dev.yml -p notes-dev up 
 elif [ "$1" == "migrate" ]; then
-    docker compose -f ./docker-compose.dev.yml exec backend python manage.py migrate
-    docker compose -f ./docker-compose.dev.yml exec backend python manage.py migrate --database=embeddings
-    docker compose -f ./docker-compose.dev.yml exec backend python manage.py migrate --database=revisions
+    docker compose -f ./docker-compose.dev.yml -p notes-dev exec backend python manage.py migrate
+    docker compose -f ./docker-compose.dev.yml -p notes-dev exec backend python manage.py migrate --database=embeddings
+    docker compose -f ./docker-compose.dev.yml -p notes-dev exec backend python manage.py migrate --database=revisions
 elif [ "$1" == "reset_admin_pw" ]; then
-    docker compose -f ./docker-compose.dev.yml exec backend python manage.py changepassword admin
+    docker compose -f ./docker-compose.dev.yml -p notes-dev exec backend python manage.py changepassword admin
 else
     echo "Usage: ./dev_tools.sh [run|migrate|reset_admin_pw]"
 fi
