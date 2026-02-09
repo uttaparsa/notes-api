338e6ad4c8b5dc73f2c1654e753a9d4b58885441
Parsa <salehipour.edu@gmai.com>
2026-02-09 02:01:16 -0600

    icons

--- next-front/app/components/FloatingNoteButton.jsx
+++ next-front/app/components/FloatingNoteButton.jsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Button } from "react-bootstrap";
+import NewIcon from "./icons/NewIcon";
 
 export default function FloatingNoteButton({ onClick, justSent = false }) {
   return (
@@ -21,19 +22,7 @@ export default function FloatingNoteButton({ onClick, justSent = false }) {
           animation: justSent ? "successPulse 0.6s ease-out" : "none",
         }}
       >
-        <svg
-          width="32px"
-          height="32px"
-          viewBox="0 0 25 25"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M17 6L19 8M14 5.5H5.5V19.5H19.5V11M9 16L9.5 13.5L19 4L21 6L11.5 15.5L9 16Z"
-            stroke="currentColor"
-            strokeWidth="1.2"
-          />
-        </svg>
+        <NewIcon />
       </Button>
 
       <style jsx>{`
---
+++ next-front/app/components/icons/NewIcon.jsx
@@ -0,0 +1,18 @@
+export default function NewIcon({ width = 32, height = 32 }) {
+  return (
+    <svg
+      width={width}
+      height={height}
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M11.25 12.75V18H12.75V12.75H18V11.25H12.75V6H11.25V11.25H6V12.75H11.25Z"
+        fill="currentColor"
+      />
+    </svg>
+  );
+}
